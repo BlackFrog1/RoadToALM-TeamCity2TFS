2b39685d7be54c11fc81d121bfbb0ebb3943b788
Rene van Osnabrugge <rvanosnabrugge@live.com>
2015-07-17 13:29:25 +0200

    Changed to Config Settings

--- RoadToALM BuildRunner/RoadToALM.Build.StartTFSBuild.v12/Constants.cs
+++ RoadToALM BuildRunner/RoadToALM.Build.StartTFSBuild.v12/Constants.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,8 @@ namespace RoadToALM.Build.StartTFSBuild.v12
 {
     public class Constants
     {
-        public const string PARAMETER_DROP_LOCATION = "RoadToALM_Droplocation";
-        public const string PARAMETER_BUILD_NAME =  "RoadtoALM_BuildName";
+        public static string PARAMETER_DROP_LOCATION = ConfigurationSettings.AppSettings["PARAMETER_DROP_LOCATION"].ToString();
+        public static string PARAMETER_BUILD_NAME = ConfigurationSettings.AppSettings["PARAMETER_BUILD_NAME"].ToString();
 
     }
 }
